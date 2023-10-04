72d03d0d7905ae18c9338e6e2b10577b66a7f831
dragunwf <plarisanmarc@gmail.com>
2023-10-04 23:01:13 +0800

    Update bot.py

--- components/bot.py
+++ components/bot.py
@@ -4,8 +4,9 @@ import json
 
 from dotenv import load_dotenv
 from pathlib import Path
+from components.data import Data
 
-client = discord.Bot()
+client = discord.Bot(intents=discord.Intents.default())
 
 
 class Bot:
@@ -18,15 +19,25 @@ class Bot:
         if message.author == client.user:
             return
         if message.author.name == "dragunwf":
-            await message.channel.send("Hello!")
+            pass
+            # await message.channel.send("Hello!")
+        print(type(message.content))
+        print(message.content)
 
     @client.event
     async def on_message_delete(message):
-        pass
+        print("delete message event")
+        print(message)
+        if message.author == client.user:
+            return
+        print("down here")
+        Data.set_previous_deleted_message(message.content)
+        print(Data.get_previous_deleted_message())
 
     @client.event
     async def on_message_edit(before, after):
-        pass
+        if before.author == client.user:
+            return
 
     @staticmethod
     def run() -> None:
