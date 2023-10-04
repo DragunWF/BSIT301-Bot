fedfd07fe4cf9c67a1a31a5d5cbef02cafc1465b
dragunwf <plarisanmarc@gmail.com>
2023-10-04 22:24:48 +0800

    Update commands.py

--- components/commands.py
+++ components/commands.py
@@ -3,7 +3,9 @@ from .data import Data
 
 
 class Commands:
-    @client.slash_command(guild_ids=Data.settings.guilds)
+    __guilds = Data.settings["guilds"]
+
+    @client.slash_command(guild_ids=__guilds)
     async def test(ctx):
         print(ctx)
         await ctx.respond("Why hello there!")
