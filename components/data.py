c1bbd082efe215f226dc2700bef2575f587374ae
dragunwf <plarisanmarc@gmail.com>
2023-10-04 22:29:14 +0800

    Update data.py

--- components/data.py
+++ components/data.py
@@ -3,7 +3,13 @@ from pathlib import Path
 
 
 class Data:
-    cwd = Path.cwd()
-    settings = json.loads(
+    __cwd = Path.cwd()
+    __settings = json.loads(
         Path(f"{Path.cwd()}/config/settings.json").read_text()
     )
+
+    def get_settings() -> dict:
+        return Data.__settings
+
+    def get_cwd() -> str:
+        return Data.__cwd
