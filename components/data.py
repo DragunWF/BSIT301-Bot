b37a2ec4030e477bb20d1e0e4b17bee126f2b3b7
dragunwf <plarisanmarc@gmail.com>
2023-10-04 22:32:22 +0800

    Update data.py

--- components/data.py
+++ components/data.py
@@ -7,9 +7,13 @@ class Data:
     __settings = json.loads(
         Path(f"{Path.cwd()}/config/settings.json").read_text()
     )
+    __previous_deleted_message = None
 
     def get_settings() -> dict:
         return Data.__settings
 
     def get_cwd() -> str:
         return Data.__cwd
+
+    def get_previous_deleted_message() -> object:
+        return Data.__previous_deleted_message
