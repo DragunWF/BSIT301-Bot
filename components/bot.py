0483b8abaea35c9ee7fdb59a17ce719d3a3d40f8
dragunwf <plarisanmarc@gmail.com>
2023-10-04 21:54:00 +0800

    Update bot.py

---
+++ components/bot.py
@@ -0,0 +1,24 @@
+import os
+import discord
+import json
+
+from dotenv import load_dotenv
+from pathlib import Path
+
+client = discord.Bot()
+
+
+class Bot:
+    @client.event
+    async def on_ready():
+        print(f"Logged in as {client.user}")
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+    @staticmethod
+    def run() -> None:
+        load_dotenv()
+        Bot.client.run(os.getenv("TOKEN"))
