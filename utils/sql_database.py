6b70d300dfcb4292dd571a24dd880ad4317c8845
Marc Plarisan <plarisanmarc@gmail.com>
2023-10-05 14:52:50 +0800

    Update sql_database.py

--- utils/sql_database.py
+++ utils/sql_database.py
@@ -1,7 +1,11 @@
 import sqlite3
+import os
 
 
 class Database:
-    @staticmethod
-    def update_stats(column: str) -> None:
-        pass
+    def __init__(self) -> None:
+        self.__path = ""
+
+    def update_stats(self) -> None:
+        with sqlite3.connect(self.__path) as db:
+            db.execute("INSERT INTO stats WHERE name = 'message_events' (?)")
