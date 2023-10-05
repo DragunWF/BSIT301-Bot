e70ed7968aa625c42ad150c02caf62ec322a6c5f
Marc Plarisan <plarisanmarc@gmail.com>
2023-10-05 08:47:17 +0800

    Update cli_log.py

--- utils/cli_log.py
+++ utils/cli_log.py
@@ -6,30 +6,31 @@ class Logger:
     __wall = "-" * 20  # For ASCII art
 
     @staticmethod
+    def __walled_print(text: str) -> None:
+        print(f"{Logger.__wall}{text}{Logger.__wall}")
+
+    @staticmethod
     def __check_channel(channel: str) -> None:
         if Logger.__channel != channel:
             Logger.__channel = channel
-            print(f"# {channel}")
+            Logger.__walled_print(f"# {channel}")
 
     @staticmethod
     @Utils.validate_args
     def log_message(content: str, author: str, channel: str) -> None:
         Logger.__check_channel(channel)
-        print(f"[{author}]: {content}")
+        Logger.__walled_print(f"[{author}]: {content}")
 
     @staticmethod
     @Utils.validate_args
     def log_deleted_message(content: str, author: str, channel: str) -> None:
         Logger.__check_channel(channel)
-        print(Logger.__wall)
-        print(f"Deleted Message by {author}\n{content}")
-        print(Logger.__wall)
+        Logger.__walled_print(f"Deleted Message by {author}\n{content}")
 
     @staticmethod
     @Utils.validate_args
     def log_edited_message(before: str, after: str, author: str, channel: str) -> None:
         Logger.__check_channel(channel)
-        print(Logger.__wall)
-        print(f"Edited Message by {author}")
-        print(f"Before Edit: {before}\n\nAfter Edit: {after}")
-        print(Logger.__wall)
+        Logger.__walled_print(
+            f"Edited Message by {author}\nBefore Edit: {before}\n\nAfter Edit: {after}"
+        )
