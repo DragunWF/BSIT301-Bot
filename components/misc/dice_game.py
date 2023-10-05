77f1608ed153b4107e01f4ef24da8364f084fdd8
Marc Plarisan <plarisanmarc@gmail.com>
2023-10-05 11:40:13 +0800

    Create dice_game.py

---
+++ components/misc/dice_game.py
@@ -0,0 +1,14 @@
+import discord
+from random import randint
+
+
+class DiceView(discord.ui.View):
+    @discord.ui.button(label="Roll The Dice!", style=discord.ButtonStyle.primary, emoji="🎲")
+    async def button_callback(self, button, interaction):
+        roll = randint(1, 6)
+        await interaction.response.send_message(f"You rolled a {roll}")
+
+
+class DiceGame:
+    def __init__(self):
+        pass
