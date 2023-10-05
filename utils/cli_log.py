b725a4aa487f20e360c253f8204639090cb279a4
Marc Plarisan <plarisanmarc@gmail.com>
2023-10-05 08:33:11 +0800

    Update cli_log.py

--- utils/cli_log.py
+++ utils/cli_log.py
@@ -2,17 +2,29 @@ from utils.utils import Utils
 
 
 class Logger:
+    __channel = None
+
+    @staticmethod
+    def __check_channel(channel: str) -> None:
+        if Logger.__channel != channel:
+            Logger.__channel = channel
+            print(f"# {channel}")
+
     @staticmethod
     @Utils.validate_args
     def log_message(content: str, author: str, channel: str) -> None:
-        pass
+        Logger.__check_channel(channel)
+        print(f"[{author}]: {content}")
 
     @staticmethod
     @Utils.validate_args
     def log_deleted_message(content: str, author: str, channel: str) -> None:
-        pass
+        Logger.__check_channel(channel)
+        print(f"Deleted Message by {author}\n{content}")
 
     @staticmethod
     @Utils.validate_args
     def log_edited_message(before: str, after: str, author: str, channel: str) -> None:
-        pass
+        Logger.__check_channel(channel)
+        print(f"Edited Message by {author}")
+        print(f"Before Edit: {before}\n\nAfter Edit: {after}")
