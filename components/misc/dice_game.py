9cf06400f6ed15e5dcaaf465666bd2155bdbf621
dragunwf <plarisanmarc@gmail.com>
2023-10-05 19:01:11 +0800

    Update dice_game.py

--- components/misc/dice_game.py
+++ components/misc/dice_game.py
@@ -6,7 +6,7 @@ class DiceView(discord.ui.View):
     @discord.ui.button(label="Roll The Dice!", style=discord.ButtonStyle.primary, emoji="🎲")
     async def button_callback(self, button, interaction):
         roll = randint(1, 6)
-        await interaction.response.send_message(f"You rolled a {roll}")
+        await interaction.response.send_message(f"You rolled a {roll} 🎲")
 
 
 class DiceGame:
