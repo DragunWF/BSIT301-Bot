15502c5b067c638e44687c6c59c9d9578b3debad
Marc Plarisan <plarisanmarc@gmail.com>
2023-10-05 14:56:23 +0800

    Update sql_database.py

--- utils/sql_database.py
+++ utils/sql_database.py
@@ -8,4 +8,20 @@ class Database:
 
     def update_stats(self) -> None:
         with sqlite3.connect(self.__path) as db:
+            # TODO: Implement stats updating from database
             db.execute("INSERT INTO stats WHERE name = 'message_events' (?)")
+
+    def update_users(self) -> None:
+        with sqlite3.connect(self.__path) as db:
+            # TODO: Implement users update for points in database
+            pass
+
+    def get_users(self) -> dict:
+        with sqlite3.connect(self.__path) as db:
+            # TODO: Implement users select query from database
+            pass
+
+    def get_stats(self) -> dict:
+        with sqlite3.connect(self.__path) as db:
+            # TODO: Implement stats select query from database
+            pass
