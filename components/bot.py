4cffd8b211b4663e141dda7a87a9fc8a48c160c6
Marc Plarisan <plarisanmarc@gmail.com>
2023-10-05 08:23:54 +0800

    Update bot.py

--- components/bot.py
+++ components/bot.py
@@ -3,6 +3,7 @@ import discord
 
 from dotenv import load_dotenv
 from components.data import Data
+from utils.cli_log import Logger
 
 client = discord.Bot(intents=discord.Intents.all())
 
@@ -16,25 +17,25 @@ class Bot:
     async def on_message(message):
         if message.author == client.user:
             return
-        if message.author.name == "dragunwf":
-            pass
-        print(message.content)
+        Logger.log_message(message.content, message.author.name)
 
     @client.event
     async def on_message_delete(message):
         if message.author == client.user:
             return
+        Logger.log_deleted_message(message.content, message.author.name)
         Data.set_deleted_message(message.content, message.author.name)
-        print("A deleted message has been stored!")
 
     @client.event
     async def on_message_edit(before, after):
         if before.author == client.user:
             return
+        Logger.log_edited_message(
+            before.content, after.content, after.author.name
+        )
         Data.set_edited_message(
             before.content, after.content, after.author.name
         )
-        print("An edited message has been stored!")
 
     @staticmethod
     def run() -> None:
