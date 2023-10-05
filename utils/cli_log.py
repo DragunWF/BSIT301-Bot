849b11acae394a08e0fe1e1634cea1ad24a87d23
Marc Plarisan <plarisanmarc@gmail.com>
2023-10-05 08:26:56 +0800

    Update cli_log.py Add channel name as a parameter to logging methods

--- utils/cli_log.py
+++ utils/cli_log.py
@@ -4,15 +4,15 @@ from utils.utils import Utils
 class Logger:
     @staticmethod
     @Utils.validate_args
-    def log_message(content: str, author: str) -> None:
+    def log_message(content: str, author: str, channel: str) -> None:
         pass
 
     @staticmethod
     @Utils.validate_args
-    def log_deleted_message(content: str, author: str) -> None:
+    def log_deleted_message(content: str, author: str, channel: str) -> None:
         pass
 
     @staticmethod
     @Utils.validate_args
-    def log_edited_message(before: str, after: str, author: str) -> None:
+    def log_edited_message(before: str, after: str, author: str, channel: str) -> None:
         pass
