c9d9709fded4dbbe5a8d8d9e7334485389759b31
Marc Plarisan <plarisanmarc@gmail.com>
2023-10-05 09:56:45 +0800

    Update commands.py

--- components/commands.py
+++ components/commands.py
@@ -8,12 +8,38 @@ from datetime import datetime
 
 class Commands:
     __guilds = Data.get_settings()["guilds"]
+    __commands = ("help", "info", "snipe", "esnipe")
 
     @client.slash_command(guild_ids=__guilds,
                           description="For testing purposes")
     async def test(ctx):
         print(ctx)
         await ctx.respond("This is a test message")
+
+    @client.slash_command(guild_ids=__guilds,
+                          description="Display all commands from BSIT301-Bot")
+    async def help(ctx):
+        command_list = "List:\n"
+        for name in Commands.__commands:
+            command_list += f"- `{name}`"
+        embed = discord.Embed(
+            title="Commands",
+            description=command_list,
+            color=discord.Colour.blurple()
+        )
+        embed.set_footer(text=Data.get_settings()["web_link"])
+        return embed
+
+    @client.slash_command(guild_ids=__guilds,
+                          description="Display information about BSIT301-Bot")
+    async def info(ctx):
+        embed = discord.Embed(
+            title="Information about BSIT301-Bot",
+            description=f"This bot is programmed and developed by {Data.get_settings()['username']}"
+        )
+        # TODO: Make a replit web server then replace this placeholder link with a real one
+        embed.set_footer(text=Data.get_settings()["web_link"])
+        await ctx.respond(embed=embed)
 
     @client.slash_command(guild_ids=__guilds,
                           description="Snipes recently deleted messages")
