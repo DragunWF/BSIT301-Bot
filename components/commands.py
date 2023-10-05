5554c05884980762e35a0bfcee0c4d85efaeb930
Marc Plarisan <plarisanmarc@gmail.com>
2023-10-05 08:09:08 +0800

    Update commands.py

--- components/commands.py
+++ components/commands.py
@@ -2,6 +2,7 @@ import discord
 
 from .bot import client
 from .data import Data
+from utils.utils import Utils
 from datetime import datetime
 
 
@@ -18,7 +19,7 @@ class Commands:
                           description="Snipes recently deleted messages")
     async def snipe(ctx):
         deleted_message = Data.get_deleted_message()
-        if deleted_message["content"] is None:
+        if deleted_message["author"] is None:
             await ctx.respond("There are no deleted messages recorded!")
         else:
             embed = discord.Embed(
@@ -29,5 +30,26 @@ class Commands:
             )
             embed.set_footer(
                 text=f"Author: {deleted_message['author']}",
+            )
+            await ctx.respond(embed=embed)
+
+    @client.slash_command(guild_ids=__guilds,
+                          description="Snipes recently edited messages")
+    @Utils.validate_args
+    async def esnipe(ctx):
+        edited_message = Data.get_edited_message()
+        if edited_message["author"] is None:
+            await ctx.respond("There are no edited messages recorded!")
+        else:
+            after = edited_message["content"]["after"]
+            before = edited_message["content"]["before"]
+            embed = discord.Embed(
+                title="Edited Message",
+                description=f"Before:\n{before}\nAfter:\n{after}",
+                color=discord.Colour.blurple(),
+                timestamp=datetime.now()
+            )
+            embed.set_footer(
+                text=f"Author: {edited_message['author']}"
             )
             await ctx.respond(embed=embed)
