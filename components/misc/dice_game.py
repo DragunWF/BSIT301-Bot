e3a601a33eb0c5c57d7c9829cf4088039a0b94ac
dragunwf <plarisanmarc@gmail.com>
2023-10-11 20:31:24 +0800

    Update dice_game.py

--- components/misc/dice_game.py
+++ components/misc/dice_game.py
@@ -6,10 +6,23 @@ from random import randint
 
 
 class DiceView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.__rolls = 0
+        self.__ROLL_LIMIT = 5
+
     @discord.ui.button(label="Roll The Dice!", style=discord.ButtonStyle.primary, emoji="🎲")
     async def button_callback(self, button, interaction):
         roll = randint(1, 6)
-        await interaction.response.send_message(f"You rolled a {roll} 🎲")
+        self.__rolls += 1
+
+        if self.__rolls == self.__ROLL_LIMIT:
+            button.disabled = True
+            button.label = "No more rolls"
+            await interaction.response.send_message("No more rolls!")
+        else:
+            await interaction.response.send_message(f"You rolled a {roll} 🎲.\n"
+                                                    + f"You have {self.__ROLL_LIMIT - self.__rolls} rolls left")
 
 
 class DiceGame:
